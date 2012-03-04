63004e0a45ed7b62506ab1f9ed10f99ef48712a7
fedorov.artiom@gmail.com <fedorov.artiom@gmail.com@2d30b940-a20a-455d-2d04-54255d48fcb8>
2012-03-04 01:17:04 +0000

    Creation de la classe perceptron

--- scan.php
+++ scan.php
@@ -6,6 +6,39 @@
 	define('PXWIDTH', 50);
 	define('PXHEIGHT', 50);
 
+	require_once("init2.php");
+	require_once("renderdata.class.php");
+	require_once("perceptron.class.php");
+
+	$perceptron = new Perceptron();
+	$perceptron->config();
+	$perceptron->init();
+	$perceptron->setLearnData($data);
+	$perceptron->train(20);
+	
+
+	
+	// construction du motif a partir du submit
+	
+	for ($i = 0; $i < 30; $i++) {
+		if (@in_array($i, $_POST['data'])) {
+			$motif[$i] = 1;
+		} else {
+			$motif[$i] = 0;
+		}
+	}
+
+
+	echo (RenderData::draw($motif));
+	
+
+
+	$answers = $perceptron->answerFormMotif($motif);
+	$scores = $perceptron->answerScoresFormMotif($motif);
+	
+	
+
+
 ?>
 
 <html>
@@ -41,7 +74,7 @@
 	<body>
 		<form method="post">
 			<div id="perceptronGrid" style="width:<? echo(NBWIDTH*PXWIDTH + (NBWIDTH * 2)) ?>;height:<? echo(NBHEIGHT * PXHEIGHT + (NBHEIGHT * 2)) ?>; border:1px solid black">
-				<? for ($i=1; $i <= NBWIDTH * NBHEIGHT ; $i++):  ?>
+				<? for ($i=0; $i < NBWIDTH * NBHEIGHT ; $i++):  ?>
 					<div class="elem" style="width:<?=PXWIDTH?>px;height:<?=PXHEIGHT?>px;border:1px solid black;float:left;<? if (in_array($i, $_POST['data'])):?>background-color:#000000;<? endif; ?> ">
 						<input type="checkbox" style="display:none" name="data[]" value="<?=$i?>"  <? if (in_array($i, $_POST['data'])):?>checked="checked"<? endif; ?> />
 					</div>
@@ -49,10 +82,38 @@
 				<div style="clear:both"></div>
 			</div>
 	
-			<div class="monitor"><? print_r($_POST['data']) ?></div>
+			<div class="monitor"><? print_r($answers) ?><br /><? print_r($scores) ?></div>
 			
 			<input type="submit" value="ok">
 			
 		</form>
+		
+		
+		
+		<?php
+		
+$r = array();
+
+echo ('<table style="border:1px solid black">');
+for ($i = 0; $i < 7; $i++) {
+	
+	echo('<tr>');
+	for($j = 0; $j < 7; $j++) {
+		$r[$i][$j] = $perceptron->calcul($j, $i);
+		if ($r[$i][$j]) {
+			$color = '#000000';
+		} else {
+			$color = '#FFFFFF';
+		}
+		echo('<td width=20 height=20 style="background-color:'.$color.';" >');
+		echo('</td>');
+	}
+	echo('</tr>');
+}
+echo ('</table>');
+		
+		?>
+		
+		
 	</body>
 </html>
