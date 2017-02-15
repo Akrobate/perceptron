69a350d75110f998d7f34fbca9215ef3a9b357bf
Artiom FEDOROV <fedorov.artiom@gmail.com>
2017-02-15 22:07:53 +0100

    scafold of dataset class. Used to be a container and toolbox for learn data

---
+++ libs/dataset.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Container and transformer
+ * of a learn / test datasets
+ * a percpetron
+ *
+ *
+ */
+
+class Dataset {
+
+    public $noise_level;
+
+    public construct() {
+
+    }
+
+}
